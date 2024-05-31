36be871e7ad99246b4c2271f93f1026672198c78
GMFields <gm.campos@campus.fct.unl.pt>
2024-05-31 18:38:45 +0200

    👌Kinda finished

--- nodes/image_frame.py
+++ nodes/image_frame.py
@@ -12,29 +12,32 @@ bridge = CvBridge()
 # Define global variables with initial values
 cx, cy = 0.0, 0.0
 
-def ImgCallBack(data):
+def ImgCallBack(data: Image) -> None:
     global cx, cy  # Declare as global to modify the global variables
     
-    # CVBRIDGE CONVERTION
+    # Convertion from ROS Image to CV Image
     cv_img = bridge.imgmsg_to_cv2(data, "bgr8")
     
     # IMAGE CROPPING
     _, width, _ = cv_img.shape
     crop_img = cv_img[240:360, 1:width]
 
-    # HSV CONVERTION
+    # Convert cropped image to HSV color space
     hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
 
-    # THRESHOLD
+    # Limits in HSV -> Only passes yellow color from line
     lower_limit = np.array([20, 100, 100])
     upper_limit = np.array([50, 255, 255])
 
+    # Create Mask, 255 bit for image between limits, 0 for those out of bounds
     mask_img = cv2.inRange(hsv_img, lower_limit, upper_limit)
 
-    # CENTROID
+    # Applies mask to original image
     res_img = cv2.bitwise_and(crop_img, crop_img, mask=mask_img)
+    # This calculates the moments of the binary mask image.
     m = cv2.moments(mask_img, False)
 
+    # Tries to finds centroid of yellow line
     try:
         cx, cy = m['m10'] / m['m00'], m['m01'] / m['m00']
     except ZeroDivisionError:
@@ -42,13 +45,13 @@ def ImgCallBack(data):
     
     print(cx, cy)
 
-    cv2.circle(res_img, (int(cx), int(cy)), 3, (0, 0, 255), -1)
+    cv2.circle(res_img, (int(cx), int(cy)), 5, (255, 0, 0), -1)
 
     # SHOW RESULTS
-    cv_img_resized = cv2.resize(cv_img, (640, 480))
-    res_img_resized = cv2.resize(res_img, (640, 480))
-    cv2.imshow("RAW", cv_img_resized)
-    cv2.imshow("FOLLOWER", res_img_resized)
+    cv_img_rsz = cv2.resize(cv_img, (640, 480))
+    res_img_rsz = cv2.resize(res_img, (640, 160))
+    cv2.imshow("CAMERA", cv_img_rsz)
+    cv2.imshow("TRACKER", res_img_rsz)
     cv2.waitKey(1)
 
 def node():
@@ -56,8 +59,8 @@ def node():
     rospy.Subscriber("/car/image_raw", Image, ImgCallBack)
     
     # PUBLISHER
-    pub = rospy.Publisher('/image_frame', Float32, queue_size=10)
-    rospy.init_node('/image_frame')
+    pub = rospy.Publisher('image_data', Float32, queue_size=15)
+    rospy.init_node('image_data')
     rate = rospy.Rate(25)  # 25Hz
 
     global cx  # Declare as global to access the global variable
--- nodes/planner.py
+++ nodes/planner.py
@@ -6,8 +6,9 @@ from std_msgs.msg import Float32
 from std_msgs.msg import Float64
 
 # Initialize constants
-MIN_VELOCITY = -2.0
-MAX_VELOCITY = 2.0
+BASE_VELOCITY = 5.0
+MIN_VARIATION = -2.0
+MAX_VARIATION = 2.0
 CENTER = 320 #Center of the RAW Resolution
 
 # PID parameters
@@ -16,13 +17,13 @@ K_I = 0.2
 K_D = 0.1
 
 # Initialize global variables
-velocity = 0
+delta_velocity = 0
 previous_error = 0
 integral_error = 0
 start_time = time.time()
 
-def PID(data):
-    global velocity
+def PID(data: Float32) -> None:
+    global delta_velocity
     global start_time
     global previous_error
     global integral_error
@@ -43,32 +44,32 @@ def PID(data):
     derivative_error = (current_error - previous_error) / dt if dt > 0 else 0
 
     # Velocity output
-    velocity = min(max((K_P * proportional_error) + (K_I * integral_error) + (K_D * derivative_error), MIN_VELOCITY), MAX_VELOCITY)
+    delta_velocity = min(max((K_P * proportional_error) + (K_I * integral_error) + (K_D * derivative_error), MIN_VARIATION), MAX_VARIATION)
 
     # Update previous error and time
     previous_error = current_error
     start_time = time.time()
 
     # Debugging prints
-    rospy.loginfo(f"current_error: {current_error}, velocity: {velocity}")
+    rospy.loginfo(f"current_error: {current_error}, velocity: {delta_velocity}")
 
 def node():
     # Initialize ROS node
     rospy.init_node('velocity', anonymous=True)
 
     # Subscriber
-    rospy.Subscriber("/image_frame", Float32, PID)
+    rospy.Subscriber("image_data", Float32, PID)
 
     # Publishers
-    pub_left = rospy.Publisher('/car/front_left_velocity_controller/command', Float64, queue_size=10)
-    pub_right = rospy.Publisher('/car/front_right_velocity_controller/command', Float64, queue_size=10)
+    pub_left = rospy.Publisher('/car/front_left_velocity_controller/command', Float64, queue_size=15)
+    pub_right = rospy.Publisher('/car/front_right_velocity_controller/command', Float64, queue_size=15)
 
     rate = rospy.Rate(25)  # 25 Hz
 
     while not rospy.is_shutdown():
         # Publish velocities
-        pub_left.publish(3 - velocity)
-        pub_right.publish(3 + velocity)
+        pub_left.publish(BASE_VELOCITY - delta_velocity)
+        pub_right.publish(BASE_VELOCITY + delta_velocity)
         rate.sleep()
 
 if __name__ == "__main__":
